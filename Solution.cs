2af02bb07df55fdc778525c54278ae90b09e59e5
Nathaniel Suchy <8227641+irlcatgirl@users.noreply.github.com>
2022-04-26 13:38:12 -0400

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+        int max = 0;
+        int count, next;
+        var set = new HashSet<int>(nums);
+        
+        foreach (var num in set) {
+            if (!(set.Contains(num-1))) {
+                count = 1;
+                next = num + 1;
+                while (set.Contains(next)) {
+                    count += 1;
+                    next += 1;
+                }
+                
+                max = Math.Max(count, max);
+            }
+        }
+        
+        return max;
+    }
+}
